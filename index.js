a58ade68b41684d88e0eb062e79aa07fc9c22120
John McMahon <jmcmahon@radiantblue.com>
2016-02-04 11:31:01 -0500

    Make GET requests work through proxy on dev env

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var express = require("express"),
+    request = require("request"),
     fs = require('fs'),
     bodyParser = require('body-parser'),
     port = process.env.PORT || 8080;
-
 
 var app = express();
 var logger = require('morgan');
@@ -18,11 +18,10 @@ app.set("view options", {
 });
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function (req, res) {
-    res.render('src/index.html');
+app.get('/proxy', function (req, res) {
+    var url = req.query.url;
+    req.pipe(request(url)).pipe(res);
 });
-
-
 
 app.listen(port);
 console.log('Express server running at http://localhost:' + port);
