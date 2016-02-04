0fa4c23671837ba3ab99235a07d2ae7e2038a75d
Jen Duncan <jduncan@radiantblue.com>
2016-02-04 13:16:57 -0500

    Name server change to make one service call instead of one for each service.

--- public/app/name-server/name-server.controller.js
+++ public/app/name-server/name-server.controller.js
@@ -11,19 +11,8 @@
     function NameServerController ($scope, $http, $log, $q, toaster) {
 
         $scope.getServices = function (){
-            $scope.getLogger();
-            $scope.getUUID();
-            $scope.getAlerter();
-            $scope.getKafka();
-            $scope.getZookeeper();
-            $scope.getSearch();
-            $scope.getIngest();
-        };
 
-        $scope.getLogger = function () {
-            $scope.loggerService = "";
-            $scope.errorMsg = "";
-            var url = 'http://pz-discover.cf.piazzageo.io/api/v1/resources/pz-logger';
+            var url = '/proxy?url=http://pz-discover.cf.piazzageo.io/api/v1/resources';
             var request = $http({
                 method: "GET",
                 url: url
@@ -31,158 +20,48 @@
             });
             request.success(
                 function( html ) {
-                    $scope.loggerHost = html.host;
-                    $scope.loggerType = html.type;
-                    $scope.loggerPort = html.port;
-                    console.log("success"+html.host);
 
+                    $scope.loggerHost = html["pz-logger"].host;
+                    $scope.loggerType = html["pz-logger"].type;
+                    $scope.loggerPort = html["pz-logger"].port;
+                    $scope.uuidHost = html["pz-uuidgen"].host;
+                    $scope.uuidType = html["pz-uuidgen"].type;
+                    $scope.uuidPort = html["pz-uuidgen"].port;
+                    $scope.alerterHost = html["pz-alerter"].host;
+                    $scope.alerterType = html["pz-alerter"].type;
+                    $scope.alerterPort = html["pz-alerter"].port;
+                    $scope.kafkaHost = html["kafka"].host;
+                    $scope.kafkaType = html["kafka"].type;
+                    $scope.kafkaPort = html["kafka"].port;
+                    $scope.zookeeperHost = html["zookeeper"].host;
+                    $scope.zookeeperType = html["zookeeper"].type;
+                    $scope.zookeeperPort = html["zookeeper"].port;
+                    $scope.searchHost = html["elasticsearch"].host;
+                    $scope.searchType = html["elasticsearch"].type;
+                    $scope.searchPort = html["elasticsearch"].port;
+                    //$scope.ingestHost = html["pzIngester"].host;
+                    //$scope.ingestType = html["pzIngester"].type;
+                    //$scope.ingestPort = html["pzIngester"].port;
+                    $scope.serviceControllerHost = html["pz-servicecontroller"].address;
+                    $scope.serviceControllerType = html["pz-servicecontroller"].type;
+                    $scope.serviceControllerPort = html["pz-servicecontroller"].port;
+                    $scope.dispatcherHost = html["pz-dispatcher"].host;
+                    $scope.dispatcherType = html["pz-dispatcher"].type;
+                    $scope.dispatcherPort = html["pz-dispatcher"].port;
+                    $scope.gatewayHost = html["pz-gateway"].host;
+                    $scope.gatewayType = html["pz-gateway"].type;
+                    $scope.gatewayPort = html["pz-gateway"].port;
+                    $scope.jobManagerHost = html["pz-jobmanager"].host;
+                    $scope.jobManagerType = html["pz-jobmanager"].type;
+                    $scope.jobManagerPort = html["pz-jobmanager"].port;
                 }
             )
             request.error(function(){
                 console.log("name-server.controller fail");
-                toaster.pop('error', "Error", "There was an issue with your request. Logger service not found.");
+                toaster.pop('error', "Error", "There was an issue with your request. One or more services not found.");
             });
+
         };
-        $scope.getUUID = function () {
-            $scope.errorMsg = "";
-            var url = 'http://pz-discover.cf.piazzageo.io/api/v1/resources/pz-uuidgen';
-            var request = $http({
-                method: "GET",
-                url: url
-
-            });
-            request.success(
-                function( html ) {
-                    $scope.uuidHost = html.host;
-                    $scope.uuidType = html.type;
-                    $scope.uuidPort = html.port;
-                    console.log("success"+html.host);
-
-                }
-            )
-            request.error(function(){
-                console.log("name-server.controller fail");
-                toaster.pop('error', "Error", "There was an issue with your request.  UUID Service not found.");
-            });
-        };
-        $scope.getAlerter = function () {
-            $scope.errorMsg = "";
-            var url = 'http://pz-discover.cf.piazzageo.io/api/v1/resources/pz-alerter';
-            var request = $http({
-                method: "GET",
-                url: url
-
-            });
-            request.success(
-                function( html ) {
-                    $scope.alerterHost = html.host;
-                    $scope.alerterType = html.type;
-                    $scope.alerterPort = html.port;
-                    console.log("success"+html.host);
-
-                }
-            )
-            request.error(function(){
-                console.log("name-server.controller fail");
-                toaster.pop('error', "Error", "There was an issue with your request. Alerter Service not found.");
-            });
-        };
-        $scope.getKafka = function () {
-            $scope.errorMsg = "";
-            var url = 'http://pz-discover.cf.piazzageo.io/api/v1/resources/kafka';
-            var request = $http({
-                method: "GET",
-                url: url
-
-            });
-            request.success(
-                function( html ) {
-                    $scope.kafkaHost = html.host;
-                    $scope.kafkaType = html.type;
-                    $scope.kafkaPort = html.port;
-                    console.log("success"+html.host);
-
-                }
-            )
-            request.error(function(){
-                console.log("name-server.controller fail");
-                toaster.pop('error', "Error", "There was an issue with your request. Kafka Service not found.");
-            });
-        };
-        $scope.getZookeeper = function () {
-            $scope.errorMsg = "";
-            var url = 'http://pz-discover.cf.piazzageo.io/api/v1/resources/zookeeper';
-            var request = $http({
-                method: "GET",
-                url: url
-
-            });
-            request.success(
-                function( html ) {
-                    $scope.zookeeperHost = html.host;
-                    $scope.zookeeperType = html.type;
-                    $scope.zookeeperPort = html.port;
-                    console.log("success"+html.host);
-
-                }
-            )
-            request.error(function(){
-                console.log("name-server.controller fail");
-                toaster.pop('error', "Error", "There was an issue with your request.  Zookeeper Service not found.");
-            });
-        };
-
-        $scope.getSearch = function () {
-            $scope.errorMsg = "";
-            var url = 'http://pz-discover.cf.piazzageo.io/api/v1/resources/elasticsearch';
-            var request = $http({
-                method: "GET",
-                url: url
-
-            });
-            request.success(
-                function( html ) {
-                    $scope.searchHost = html.host;
-                    $scope.searchType = html.type;
-                    $scope.searchPort = html.port;
-                    console.log("success"+html.host);
-
-                }
-            )
-            request.error(function(){
-                console.log("name-server.controller fail");
-                toaster.pop('error', "Error", "There was an issue with your request.  Search Service not found.");
-            });
-        };
-
-        $scope.getIngest = function () {
-            $scope.errorMsg = "";
-            var url = 'http://pz-discover.cf.piazzageo.io/api/v1/resources/pz-ingester';
-            var request = $http({
-                method: "GET",
-                url: url
-
-            });
-            request.success(
-                function( html ) {
-                    $scope.ingestHost = html.host;
-                    $scope.ingestType = html.type;
-                    $scope.ingestPort = html.port;
-                    console.log("success"+html.host);
-
-                }
-            )
-            request.error(function(){
-                console.log("name-server.controller fail");
-                toaster.pop('error', "Error", "There was an issue with your request.  Ingest Service not found.");
-            });
-        };
-
-
-
-
-
-
     }
 
 })();
