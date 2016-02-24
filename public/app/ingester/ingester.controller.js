d7d34eb5493bb432e09ce1c6b9f63446017e7abe
John McMahon <jmcmahon@radiantblue.com>
2016-02-24 13:46:39 -0500

    Text ingest working

--- public/app/ingester/ingester.controller.js
+++ public/app/ingester/ingester.controller.js
@@ -24,88 +24,102 @@
         $scope.data = "none";
 
         $scope.ingest = function () {
-            var f = document.getElementById('file').files[0],
+            /*var f = document.getElementById('file').files[0],
                 r = new FileReader();
             r.onloadend = function (e) {
                 $scope.data = e.target.result;
+            };
+            r.readAsBinaryString(f);*/
+
+            var data = {};
+            if (angular.isDefined($scope.ingestType) && $scope.ingestType == "Text") {
+                if (angular.isUndefined($scope.message) || $scope.message == "") {
+                    toaster.pop('warning', "Missing Required Field", "Must include Text to ingest.");
+                    return;
+                }
+                data = {
+                    "dataType": {
+                        "type": "text",
+                        "mimeType": "application/text",
+                        "content": $scope.message
+                    },
+                    "metadata": {
+                        "name": "Testing some SAK ingester",
+                        "description": "This is a UI Test.",
+                        "classType": "unclassified"
+                    }
+                };
+            } else if (angular.isDefined($scope.ingestType) && $scope.ingestType == "File") {
+                if (angular.isUndefined($scope.file) || $scope.file == "") {
+                    toaster.pop('warning', "Missing Required Field", "Must include one file to upload.");
+                    return;
+                }
+                data = {
+                    "dataType": {
+                        "type": "file",
+                        "mimeType": "image/jpeg",
+                        "content": $scope.file
+                    },
+                    "metadata": {}
+                };
             }
-            r.readAsBinaryString(f);
-
-
             var ingestObj = {
-                "apiKey": "my-api-key-929304",
+                "apiKey": "my-api-key-sakui",
                 "jobType": {
                     "type": "ingest",
                     "host": "true",
-                    "data" : {
-                        "dataType": {
-                            "type": "text",
-                            "mimeType": "application/text",
-                            "content": "This text itself is the raw data to be ingested. In reality, it could be some GML, or GeoJSON, or whatever."
-                        },
-                        "metadata": {
-                            "name": "Testing some Text Input",
-                            "description": "This is a test.",
-                            "classType": "unclassified"
-                        }
-                    }
+                    "data": data
                 }
             };
 
             // TODO: Either include the JSON in the url as a param or include it in the form data
             var fd = new FormData();
-            fd.append( 'data', ingestObj );
-            fd.append( 'file', f );
+            fd.append( 'body',  JSON.stringify(ingestObj) );
+            // fd.append( 'file', f );
 
-            $.ajax({
-                url: '/proxy?url=pz-gateway.cf.piazzageo.io/job',
-                data: ingestObj,
-                processData: false,
-                contentType: false,
-                type: 'POST',
-                success: function(data){
-                    $scope.jobId = data.jobId;
-                    alert(data);
-                    toaster.pop('success', "Success", "Ingest was a success");
-                },
-                error: function(res) {
-                    //$scope.errorMsg = res.status;
-                    console.log("ingester.controller fail"+res.status);
-                    toaster.pop('error', "Error", "There was an error with file ingest.");
-                    //alert("Error " + res.status);
+            $http({
+                method: "POST",
+                url: "/proxy?url=pz-gateway.cf.piazzageo.io/job",
+                data: fd,
+                headers: {
+                    "Content-Type": undefined
                 }
+            }).then(function successCallback( html ) {
+                $scope.jobIdResult = html.data.jobId;
+            }, function errorCallback(response){
+                console.log("search.controller fail");
+                toaster.pop('error', "Error", "There was an issue with your request.");
             });
-            /*$.post("urlToCome", function () {
-                    alert("success");
-                })
-                .done(function () {
-                    alert("second success");
-                })
-                .fail(function () {
-                    alert("error");
-                })
-                .always(function () {
-                    alert("finished");
-                });*/
-           // $scope.jobId = "39492023940958201209348";
-        }
+        };
 
         $scope.getJobStatus = function() {
+            if (angular.isUndefined($scope.jobId) || $scope.jobId == "") {
+                toaster.pop('warning', "Missing Required Field", "Must include Job ID.");
+                return;
+            }
+            var data = {
+                "apiKey": "my-api-key-sak",
+                "jobType": {
+                    "type": "get",
+                    "jobId": $scope.jobId
+                }
+            };
+
+            var fd = new FormData();
+            fd.append( 'body', JSON.stringify(data) );
+
             $http({
-                method: "GET",
-                url: "/proxy?url=pz-discover.cf.piazzageo.io/api/v1/resources/pz-gateway"
-            }).then(function(result) {
-
-                $http({
-                    method: "GET",
-                    url: "/proxy?url=" + result.data.host + "/job",
-                }).then(function successCallback( html ) {
-                    $scope.jobStatus = html.data;
-                }, function errorCallback(response){
-                    console.log("Ingester.controller fail"+response.status);
-                    toaster.pop('error', "Error", "There was an issue with your request.");
-                });
-
+                method: "POST",
+                url: "/proxy?url=pz-gateway.cf.piazzageo.io/job",
+                data: fd,
+                headers: {
+                    "Content-Type": undefined
+                }
+            }).then(function successCallback( html ) {
+                $scope.jobStatus = html.data;
+            }, function errorCallback(response){
+                console.log("search.controller fail");
+                toaster.pop('error', "Error", "There was an issue with your request.");
             });
         }
     }
