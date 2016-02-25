cf3360b9fbb37e661ce1594db300dacada777414
John McMahon <jmcmahon@radiantblue.com>
2016-02-24 20:32:13 -0500

    First crack at tabbed panels

--- public/app/logger/logger.controller.js
+++ public/app/logger/logger.controller.js
@@ -55,7 +55,7 @@
                 time: currentTime,
                 severity: "Info",
                 message: logMessage
-            }
+            };
             $http({
                 method: "GET",
                 url: "/proxy?url=pz-discover.cf.piazzageo.io/api/v1/resources/pz-logger"
@@ -77,26 +77,9 @@
                     //$scope.successMsg = "There was a problem submitting the Log Message."
                    toaster.pop('error', "Error", "There was a problem submitting the log message.");
                 });
-            })
-        }
-
-
-
-
-        $scope.tabs = [
-            { title:'Dynamic Title 1', content:'Dynamic content 1' },
-            { title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
-        ];
-
-        $scope.alertMe = function() {
-            setTimeout(function() {
-                $window.alert('You\'ve selected the alert tab!');
             });
         };
 
-        $scope.model = {
-            name: 'Tabs'
-        };
     }
 
 })();
