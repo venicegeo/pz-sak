f6324708f5fe741cca1285a4547d8dbcd35e8e99
John McMahon <jmcmahon@radiantblue.com>
2016-06-07 21:04:25 -0400

    3823 Delete files that are no longer needed

--- public/app/wfs/app.module.wfs.js
+++
@@ -1,7 +0,0 @@
-// This file is still referenced in index.html but app isn't referenced anywhere
-// Might be able to safely get rid of this
-(function(){
-    'use strict';
-    angular.module('app',['angularSpinner']);
-
-})();
