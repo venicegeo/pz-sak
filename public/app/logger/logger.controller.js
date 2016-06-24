138a26b7f1321a477ed48be98c305f3034efdbe3
John McMahon <jmcmahon@radiantblue.com>
2016-06-24 09:42:05 -0400

    4245 Update paging for logger

--- public/app/logger/logger.controller.js
+++ public/app/logger/logger.controller.js
@@ -17,32 +17,36 @@
     'use strict';
     angular
         .module('SAKapp')
-        .controller('LoggerController', ['$scope', '$http', '$log', '$q',  'toaster', 'discover', LoggerController]);
+        .controller('LoggerController', ['$scope', '$http',  'toaster', 'discover', 'usSpinnerService', LoggerController]);
 
-    function LoggerController ($scope, $http, $log, $q, toaster, discover) {
+    function LoggerController ($scope, $http, toaster, discover, usSpinnerService) {
         $scope.pageOptions = [10, 50, 100, 500];
         $scope.size=100;
-        $scope.from=0;
 
-        $scope.$watch("size", function(newValue, oldValue) {
-            $scope.from=0;
-            $scope.getLogs();
-        });
+        $scope.pagination = {
+            current: 0
+        };
+        
+        $scope.pageChanged = function(newPage) {
+            $scope.getLogs(newPage);
+        };
 
         $scope.showHideSearchForm = function() {
             $scope.showSearchLogs = !$scope.showSearchLogs;
         };
 
-
         $scope.searchLogs = function() {
-            //TODO:Once Logger Search API is updated, we need to update this call to pass search params and show only what is returned.
             $scope.getLogs();
         };
 
-        $scope.getLogs = function () {
+        $scope.getLogs = function (pageNumber) {
+            usSpinnerService.spin('spinner');
+            if (pageNumber) {
+                $scope.pagination.current = pageNumber - 1;
+            }
             var params = {
                 per_page : $scope.size,
-                page : $scope.from
+                page : $scope.pagination.current
             };
             if ($scope.afterDate) {
                 angular.extend(params, {
@@ -72,10 +76,11 @@
                 url: "/proxy/" + discover.loggerHost + "/v2/message",
                 params: params
             }).then(function successCallback( html ) {
+                usSpinnerService.stop('spinner');
                 $scope.logs = html.data.data;
                 $scope.logCount = html.data.pagination.count;
-
             }, function errorCallback(response){
+                usSpinnerService.stop('spinner');
                 console.log("logger.controller get logs fail: "+response.status);
                 toaster.pop('error', "Error", "There was an issue with retrieving the logs.");
             });
@@ -109,26 +114,16 @@
             });
         };
 
-        $scope.nextPage = function() {
-            if ($scope.from < $scope.logCount-$scope.size) {
-                $scope.from += $scope.size;
-                $scope.getLogs();
-            }
+        $scope.getFirstIndex = function () {
+            return ($scope.pagination.current * $scope.size) + 1;
         };
 
-        $scope.prevPage = function() {
-            if ($scope.from > 0) {
-                $scope.from -= $scope.size;
-                $scope.getLogs();
+        $scope.getLastIndex = function () {
+            var end = ($scope.pagination.current * $scope.size) + $scope.size;
+            if (end > $scope.logCount) {
+                return $scope.logCount;
             }
-        };
-
-        $scope.getLastIndex = function() {
-            var endingPoint = $scope.from + $scope.size;
-            if (endingPoint > $scope.logCount) {
-                endingPoint = $scope.logCount;
-            }
-            return endingPoint;
+            return end;
         };
 
     }
