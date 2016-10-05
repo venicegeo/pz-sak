4fe05ad03798c5d7dac531bb5d6461ea9f85f76b
John McMahon <jmcmahon@radiantblue.com>
2016-10-05 13:24:35 -0400

    Add coverage reports to karma tests

--- karma.conf.js
+++ karma.conf.js
@@ -1,3 +1,18 @@
+/**
+ Copyright 2016, RadiantBlue Technologies, Inc.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 // Karma configuration
 // Generated on Fri May 20 2016 09:17:02 GMT-0400 (Eastern Daylight Time)
 
@@ -6,7 +21,6 @@ module.exports = function(config) {
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
-
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
@@ -33,14 +47,20 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+        'public/app/**/!(*test).js': 'coverage'
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress','coverage'],
 
+    coverageReporter : {
+      type : 'text-summary',
+      dir : 'coverage/',
+      file : 'textsummary.txt'
+    },
 
     // web server port
     port: 9876,
@@ -67,7 +87,7 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
 
     // Concurrency level
     // how many browser should be started simultaneous
