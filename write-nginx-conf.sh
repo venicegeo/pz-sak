03b0de457ff8f1ead52b23516682ac54414f82fc
Ben Hosmer <ben.hosmer@gmail.com>
2017-11-02 15:49:22 -0400

    Fix nginx config crash

--- write-nginx-conf.sh
+++ write-nginx-conf.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
+
 resolvers=(`grep nameserver /etc/resolv.conf | awk '$2 != "127.0.0.1" {print $2}' | head -n1`)
-cat > ./resolvers.conf << EOM
+cat > $HOME/public/nginx.conf << EOM
 resolver ${resolvers[*]} valid=30s;
 EOM
