d55dddc5749e7e7c24fe138ba26b84131df7b2cb
Ben Hosmer <ben.hosmer@gmail.com>
2017-11-03 08:55:58 -0400

    Fix unknown crash

--- write-nginx-conf.sh
+++ write-nginx-conf.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 resolvers=(`grep nameserver /etc/resolv.conf | awk '$2 != "127.0.0.1" {print $2}' | head -n1`)
-sed -i "s/resolver.*/resolver ${resolvers[*]} valid=30s;/g" $HOME/public/nginx.conf
+cat > ./resolvers.conf << EOM
+resolver ${resolvers[*]} valid=30s;
+EOM
