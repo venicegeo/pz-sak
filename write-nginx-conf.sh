9a6b79a3cf6a596372d76aeae6992d4ba7eb5254
Ben Hosmer <ben.hosmer@gmail.com>
2017-11-03 08:40:11 -0400

    Debug blank error

--- write-nginx-conf.sh
+++ write-nginx-conf.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 resolvers=(`grep nameserver /etc/resolv.conf | awk '$2 != "127.0.0.1" {print $2}' | head -n1`)
-cat > $HOME/public/nginx.conf << EOM
-resolver ${resolvers[*]} valid=30s;
-EOM
+sed -i "s/resolver.*/resolver ${resolvers[*]} valid=30s;/g" $HOME/public/nginx.conf
